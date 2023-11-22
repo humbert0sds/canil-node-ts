179714116fe5e5c7da58f584fcc7318849916857
humberto <contagithubHsds@gmail.com>
2023-11-22 20:06:15 -0300

    '.'

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-    res.send('HOME CONTROLLER')
+    res.render('pages/page')
 }
 export const cachorros = (req: Request, res: Response) => {
     res.send('CACHORROS CONTROLLER')
