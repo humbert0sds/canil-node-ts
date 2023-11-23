83164dff144ca2ad7225ac006a0df6f8d5b76168
humberto <contagithubHsds@gmail.com>
2023-11-23 14:49:02 -0300

    '.'

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,40 @@
 import { Request, Response } from "express";
+import { createMenuObject } from '../helpers/createMenuObject'
 
 export const home = (req: Request, res: Response) => {
-    res.render('pages/page')
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    })
 }
 export const cachorros = (req: Request, res: Response) => {
-    res.send('CACHORROS CONTROLLER')
+
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    })
 }
 export const gatos = (req: Request, res: Response) => {
-    res.send('GATOS CONTROLLER')
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    })
 }
 export const peixes = (req: Request, res: Response) => {
-    res.send('PEIXES CONTROLLER')
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    })
 }
